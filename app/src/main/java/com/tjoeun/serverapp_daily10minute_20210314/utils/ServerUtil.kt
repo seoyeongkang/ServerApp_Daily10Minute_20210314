55212d8e5666e5b0f66c91249c0f2c59abd769c4
seoyeongkang <jumprope123@naver.com>
2021-03-14 17:03:09 +0900

    연습 - 서버 응답 코드를 보고 로그인 성공/실패 로그 찍기

--- app/src/main/java/com/tjoeun/serverapp_daily10minute_20210314/utils/ServerUtil.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minute_20210314/utils/ServerUtil.kt
@@ -72,6 +72,18 @@ class ServerUtil {
                     val jsonObj = JSONObject(bodyString)
                     Log.d("서버 응답 본문", jsonObj.toString())
 
+//                    연습 : code에 적힌 숫자 (Int)가 얼마 인가?
+
+                    val codeNum = jsonObj.getInt("code")
+
+//                    연습 활용 : codeNum 200이면, 로그인 성공. 아니면 로그인 실패. 로그 찍기
+
+                    if(codeNum == 200){
+                        Log.d("로그인 결과", "성공")
+                    }
+                    else {
+                        Log.e("로그인 결과", "실패")
+                    }
                 }
 
 
