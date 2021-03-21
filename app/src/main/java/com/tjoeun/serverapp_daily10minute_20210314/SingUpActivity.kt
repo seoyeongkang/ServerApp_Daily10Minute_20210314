b4d1dd5f78377c3312ad60bc700227441b9e65e5
seoyeongkang <jumprope123@naver.com>
2021-03-21 10:01:19 +0900

    회원 가입 화면 추가

---
+++ app/src/main/java/com/tjoeun/serverapp_daily10minute_20210314/SingUpActivity.kt
@@ -0,0 +1,20 @@
+package com.tjoeun.serverapp_daily10minute_20210314
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SingUpActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sing_up)
+
+        setupEvents()
+        setValues()
+    }
+    override fun setupEvents() {
+
+    }
+    override fun setValues() {
+
+    }
+}
