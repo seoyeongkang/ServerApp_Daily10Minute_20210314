779bb7101e08e908ea10bbb2c6a81f88a5bea392
seoyeongkang <jumprope123@naver.com>
2021-03-21 10:22:28 +0900

    회원가입 클릭시 입력 데이터 받아오기

--- app/src/main/java/com/tjoeun/serverapp_daily10minute_20210314/SingUpActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minute_20210314/SingUpActivity.kt
@@ -2,6 +2,11 @@ package com.tjoeun.serverapp_daily10minute_20210314
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.emailEdt
+import kotlinx.android.synthetic.main.activity_main.pwEdt
+import kotlinx.android.synthetic.main.activity_main.signUpBtn
+import kotlinx.android.synthetic.main.activity_sing_up.*
 
 class SingUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +19,17 @@ class SingUpActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        signUpBtn.setOnClickListener {
+//            이메일/비번/닉네임 => 서버 회원 가입 기능에 전송
+
+            val email = emailEdt.text.toString()
+            val pw = pwEdt.text.toString()
+            val nick = nickNameEdt.text.toString()
+            
+//             서버 - 회원가입 기능에 전송 : serverUtil에 회원가입 함수 필요
+
+        }
+         
     }
 
     override fun setValues() {
