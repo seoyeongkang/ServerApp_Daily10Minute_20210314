0c4f6df85058e5ab2a7c68a44c5c294952c3aff4
seoyeongkang <jumprope123@naver.com>
2021-03-28 15:20:16 +0900

    서버에 프로젝트 인우너 조회 API 호출까지

--- app/src/main/java/com/tjoeun/serverapp_daily10minute_20210314/ViewMemberListActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minute_20210314/ViewMemberListActivity.kt
@@ -3,10 +3,15 @@ package com.tjoeun.serverapp_daily10minute_20210314
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.tjoeun.serverapp_daily10minute_20210314.data.Project
+import com.tjoeun.serverapp_daily10minute_20210314.data.User
+import com.tjoeun.serverapp_daily10minute_20210314.utils.ServerUtil
+import org.json.JSONObject
 
 class ViewMemberListActivity : BaseActivity() {
 
     lateinit var mProject : Project
+
+    val mMemberList = ArrayList<User>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +28,14 @@ class ViewMemberListActivity : BaseActivity() {
     override fun setValues() {
 
         mProject = intent.getSerializableExtra("project") as Project
+
+//        서버에서 참여인원이 누구누구 있는지 요청 => 파싱 => mMemberList 에 추가(add)
+        ServerUtil.getRequestProjectMembers(mContext, mProject.id, object  : ServerUtil.JsonResponseHandler {
+            override fun onResponse(json: JSONObject) {
+
+            }
+
+        })
 
     }
 
