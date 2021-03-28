a8c37de8171deb61f153b3c3c0b8456f2cd38903
seoyeongkang <jumprope123@naver.com>
2021-03-28 17:04:12 +0900

    ViewPager 이벤트 처리 완료  - 페이지 옮기면 몇페이지 인지 텍스트뷰 반영

--- app/src/main/java/com/tjoeun/serverapp_daily10minute_20210314/ViewPhotoActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minute_20210314/ViewPhotoActivity.kt
@@ -2,6 +2,7 @@ package com.tjoeun.serverapp_daily10minute_20210314
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
 import com.bumptech.glide.Glide
 import com.tjoeun.serverapp_daily10minute_20210314.adapters.PhotoViewPagerAdaper
 import com.tjoeun.serverapp_daily10minute_20210314.data.User
@@ -23,6 +24,32 @@ class ViewPhotoActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+//        뷰페이저의 페이지 변경되었을때 => 이벤트 처리
+
+        profilePhotoViewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
+            override fun onPageScrollStateChanged(state: Int) {
+
+
+            }
+
+            override fun onPageSelected(position: Int) {
+
+//                페이지를 완전히 넘겼을때 실행되는 코드
+//                현재 페이지 / 전체 장수 => ex. 2 / 5 텍스트로 반영
+
+                photoCountTxt.text = "${position+1} / ${mUser.profileImgUrls.size}"
+            }
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+
+            }
+
+        })
+
     }
 
     override fun setValues() {
@@ -31,6 +58,9 @@ class ViewPhotoActivity : BaseActivity() {
 
         mPhotoAdapter = PhotoViewPagerAdaper(supportFragmentManager, mUser.profileImgUrls)
         profilePhotoViewPager.adapter = mPhotoAdapter
+
+//        사진이 실제로 몇장인지 받아서 텍스트뷰에 반영.
+        photoCountTxt.text = "1 / ${mUser.profileImgUrls.size}"
 
     }
 }
