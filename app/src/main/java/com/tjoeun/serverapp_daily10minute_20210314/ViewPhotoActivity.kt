989024db1d369b32ebb3aaa8b5d3f73a6bc8eb8d
seoyeongkang <jumprope123@naver.com>
2021-03-28 16:54:48 +0900

    액티비티에서 ViewPager 어댑터 적용  - 프로필 사진 목록도 같이 전달해서 적용

--- app/src/main/java/com/tjoeun/serverapp_daily10minute_20210314/ViewPhotoActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minute_20210314/ViewPhotoActivity.kt
@@ -3,12 +3,15 @@ package com.tjoeun.serverapp_daily10minute_20210314
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
+import com.tjoeun.serverapp_daily10minute_20210314.adapters.PhotoViewPagerAdaper
 import com.tjoeun.serverapp_daily10minute_20210314.data.User
 import kotlinx.android.synthetic.main.activity_view_photo.*
 
 class ViewPhotoActivity : BaseActivity() {
 
     lateinit var mUser : User
+
+    lateinit var mPhotoAdapter : PhotoViewPagerAdaper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +29,8 @@ class ViewPhotoActivity : BaseActivity() {
 
         mUser = intent.getSerializableExtra("user") as User
 
-
+        mPhotoAdapter = PhotoViewPagerAdaper(supportFragmentManager, mUser.profileImgUrls)
+        profilePhotoViewPager.adapter = mPhotoAdapter
 
     }
 }
