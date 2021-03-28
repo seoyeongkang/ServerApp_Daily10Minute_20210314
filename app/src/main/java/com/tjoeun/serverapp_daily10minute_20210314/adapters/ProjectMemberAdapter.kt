5415126b9e075b90af2f508502d00fdeb84ecdcf
seoyeongkang <jumprope123@naver.com>
2021-03-28 15:10:09 +0900

    프로젝트 참여 인원 표시 어댑터 뼈대작업

---
+++ app/src/main/java/com/tjoeun/serverapp_daily10minute_20210314/adapters/ProjectMemberAdapter.kt
@@ -0,0 +1,33 @@
+package com.tjoeun.serverapp_daily10minute_20210314.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.tjoeun.serverapp_daily10minute_20210314.R
+import com.tjoeun.serverapp_daily10minute_20210314.data.User
+
+class ProjectMemberAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : ArrayList<User>) : ArrayAdapter<User>(mContext, resId, mList) {
+
+    val inflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if(tempRow == null){
+            tempRow = inflater.inflate(R.layout.member_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+
+        return row
+
+    }
+
+}
