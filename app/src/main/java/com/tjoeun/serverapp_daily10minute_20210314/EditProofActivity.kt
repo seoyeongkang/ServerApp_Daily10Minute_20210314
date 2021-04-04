f9b3c5f357bc070f7fe1ba7c3965307bd46c4c3f
seoyeongkang <jumprope123@naver.com>
2021-04-04 15:37:52 +0900

    인증글 작성 화면 진입

---
+++ app/src/main/java/com/tjoeun/serverapp_daily10minute_20210314/EditProofActivity.kt
@@ -0,0 +1,26 @@
+package com.tjoeun.serverapp_daily10minute_20210314
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tjoeun.serverapp_daily10minute_20210314.data.Project
+
+class EditProofActivity : BaseActivity() {
+
+    lateinit var mProject : Project
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_proof)
+        setupEvents()
+        setValues()
+
+    }
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+        mProject = intent.getSerializableExtra("project") as Project
+    }
+}
