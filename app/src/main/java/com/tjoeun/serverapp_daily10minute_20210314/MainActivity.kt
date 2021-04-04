5773d374dea3756791047974b64062f41628c2cc
seoyeongkang <jumprope123@naver.com>
2021-04-04 10:02:19 +0900

    메인화면에서 액션바 적용  - 흰색 배경 추가  - 좌우 여백 제거 작업 필요

--- app/src/main/java/com/tjoeun/serverapp_daily10minute_20210314/MainActivity.kt
+++ app/src/main/java/com/tjoeun/serverapp_daily10minute_20210314/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
         setupEvents()
         setValues()
+        setCustomActionBar()
     }
 
     override fun setupEvents() {
